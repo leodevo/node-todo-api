c82096fd0dc5efec5b5bf805a77169b19c515b96
leodevo <leodevogithub@gmail.com>
2018-11-19 18:37:10 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+const {ObjectID} = require('mongodb')
 
 var { mongoose } = require('./db/mongoose')
 var { Todo } = require('./models/todo')
@@ -28,6 +29,27 @@ app.get('/todos', (req, res) => {
     res.send({ todos })
   }, (e) => {
     res.status(400).send(e)
+  })
+})
+
+// GET /todos /1234324
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id
+
+  if (!ObjectID.isValid(id)) {
+    console.log('ID not valid')
+    return res.status(404).send()
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      console.log('Id not found')
+      return res.status(404).send()
+    } else {
+      return res.send({ todo })
+    }
+  }).catch((e) => {
+    return res.status(400).send()
   })
 })
 
