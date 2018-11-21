60269b27bc87efdcddfcc93f41835c6ab8a60cb3
leodevo <leodevogithub@gmail.com>
2018-11-21 10:42:46 +0100

    small modifications + adding package-lock.json

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,17 +1,16 @@
 var mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-//mongoose.connect('mongodb://localhost:27017/TodoApp')
 
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
   mlab: 'mongodb://<user>:<password>.mlab.com:<number>/<account>'
-};
-//|| db.mlab db.localhost ||
+}
+
 mongoose.connect(db.mlab || db.localhost).then(() => {
   console.log('connected to mongoDB service')
-}, function(err) {
-  console.log(err);
+}, (err) => {
+  console.log(err)
 })
 
 module.exports = { mongoose }
