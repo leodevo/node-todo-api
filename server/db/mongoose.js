7c12069119c6ca9c4fd0788b3d19cf51accd20fc
leodevo <leodevogithub@gmail.com>
2018-11-21 11:21:07 +0100

    DB URI modified

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise
 
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: MONGODB_URI
+  mlab: process.env.MONGODB_URI
 }
 
 mongoose.connect(db.mlab || db.localhost).then(() => {
