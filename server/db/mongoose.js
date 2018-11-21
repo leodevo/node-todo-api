1033268f9a8305ad813aa5b1fec0e51256133cce
leodevo <leodevogithub@gmail.com>
2018-11-21 11:08:35 +0100

    modifying URL for DB connexion

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise
 
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://<user>:<password>.mlab.com:<number>/<account>'
+  mlab: MONGODB_URI
 }
 
 mongoose.connect(db.mlab || db.localhost).then(() => {
