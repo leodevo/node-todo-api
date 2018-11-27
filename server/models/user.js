5df602fcdae1784a7ea3533272c26876f88ac6ed
leodevo <leodevogithub@gmail.com>
2018-11-27 12:12:29 +0100

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,60 @@
-var mongoose = require('mongoose')
+const mongoose = require('mongoose')
+const validator = require('validator')
+const jwt = require('jsonwebtoken')
+const _ = require('lodash')
 
-var User = mongoose.model('User', {
+let UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     minlength: 1,
-    trim: true
-  }
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 })
+
+//override method :) 
+UserSchema.methods.toJSON = function () {
+  let user = this
+  let userObject = user.toObject() //converts mongoose object to a regular object
+
+  return _.pick(userObject, ['_id', 'email']) // do not return password and token
+}
+
+//here defining function with 'function' keyword and not an arrow function because arrow function do not bind 'this'
+//but if I define it with the function keyword, if I call this method on a object user, then the keyword this will be bind
+// to that object. With an arrow function it could be bind to anything.
+UserSchema.methods.generateAuthToken = function () {
+  let user = this
+  let access = 'auth'
+  let token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString()
+
+  user.tokens = user.tokens.concat([{ access, token }])
+
+  return user.save().then(() => {
+    return token
+  })
+}
+
+var User = mongoose.model('User', UserSchema) 
 
 module.exports = { User }
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ require('./config/config')
 const _ = require('lodash')
 const express = require('express')
 const bodyParser = require('body-parser')
-const {ObjectID} = require('mongodb')
+const { ObjectID } = require('mongodb')
 
 var { mongoose } = require('./db/mongoose')
 var { Todo } = require('./models/todo')
@@ -95,6 +95,20 @@ app.patch('/todos/:id', (req, res) => {
 
     res.send({ todo })
   }).catch((e) => res.status(400).send())
+})
+
+//POST / users
+app.post('/users', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password'])
+  let user = new User(body)
+
+  user.save().then(() => {
+    return user.generateAuthToken()
+  }).then((token) => {
+    res.header('x-auth', token).send(user)
+  }).catch((e) => {
+    res.status(400).send(e)
+  })
 })
 
 app.listen(port, () => {
