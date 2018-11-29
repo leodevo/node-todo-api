17d81daec578e40ebd54b3099b1fdf54ab13a2b0
leodevo <leodevogithub@gmail.com>
2018-11-29 11:09:11 +0100

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,22 @@
 const { SHA256 } = require('crypto-js')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
+let password = '123abc!'
+/*
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash)
+  }) 
+})*/
+
+
+var hashedPassword = '$2a$10$FH0JohReYuWakAdWnRe9JO4Q.MK.xhCK68Xxag5j362V52IRGnOjG'
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res)
+})
+/*
 let data = {
   id: 10
 }
@@ -9,7 +25,11 @@ let token = jwt.sign(data, '123abc')
 console.log(token)
 
 var decoded = jwt.verify(token, '123abc')
-console.log(decoded)
+console.log(decoded)*/
+
+// ----------------------------------------------------------
+
+
 //jwt.verify
 // let message = 'I am user number 3'
 // let hash = SHA256(message).toString()
--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -3,3 +3,4 @@ const {ObjectId} = require('mongodb')
 const {mongoose} = require('./../server/db/mongoose')
 const {Todo} = require('./../server/models/todo')
 const {User} = require('./../server/models/user')
+
