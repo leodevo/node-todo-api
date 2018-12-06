5a2bb393072eb4f485e86ee68aa395decd76a3e2
leodevo <leodevogithub@gmail.com>
2018-12-06 20:23:15 +0100

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -268,7 +268,7 @@ describe('POST /users/login', () => {
 
         User.findById(users[1]._id).then((user) => {
           expect(user.tokens[0]).toMatchObject({
-            access: 'authd',
+            access: 'auth',
             token: res.headers['x-auth']
           });
           done();
@@ -300,3 +300,22 @@ describe('POST /users/login', () => {
   })
 })
 
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .send()
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0)
+          done()
+        }).catch((e) => { done(e) })
+      })
+  });
+})
