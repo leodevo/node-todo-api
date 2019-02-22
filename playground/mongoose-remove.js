6769b53fd248c8f62fc5d9f0e8ee016bbb65ec23
leodevo <git>
2019-02-22 11:20:18 +0100

    Ignoring playground/

--- playground/mongoose-remove.js
+++
@@ -1,6 +0,0 @@
-const {ObjectId} = require('mongodb')
-
-const {mongoose} = require('./../server/db/mongoose')
-const {Todo} = require('./../server/models/todo')
-const {User} = require('./../server/models/user')
-
